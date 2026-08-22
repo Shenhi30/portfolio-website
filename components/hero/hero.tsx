a3d8a8ba1be37b7c614d683aa62976dba33fa59a
Viktor <shenhi0master@gmail.com>
2026-08-22 16:57:12 +0300

    Update hero.tsx

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -9,25 +9,26 @@ export default function Hero() {
         <section className={css.Hero}>
             <div className={css.text_box}>
                 <h1 className={css.home_header}>Viktor is a <span className={css.text_purple}>front-end developer</span></h1>
-                <p className={css.home_text}>aspiring front-end developer with 1 year expirence</p>
-                <Link href="/contacts" className={css.contact_btn}>Contact me !</Link>
+                <p className={css.home_text}>front-end developer with 1 year expirence</p>
+                <Link href="/contacts" className={css.contact_btn}>Contact me!!</Link>
             </div>
             <div className={css.image_box}>
-              <Image
-                src="/images/person.png"
-                alt='person'
-                width={457}
-                height={386}
-                priority
-              />
-                <div className={css.under_text}>
-                    <p>
-                    <span className={css.box}></span>
-                    Currently working on
-                    <span className={css.white_text}>Portfolio
-                    </span>
-                    </p>
+                <div className={css.imageWrapper}>
+                    <Image
+                    src="/images/person.png"
+                    alt='person'
+                    width={457}
+                    height={386}
+                    priority
+                    />
                 </div>
+                    <div className={css.under_text}>
+                        <p>
+                        Currently working on
+                        <span className={css.white_text}>Portfolio
+                        </span>
+                        </p>
+                    </div>
             </div>
         </section>
         </>
